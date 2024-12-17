16f0d7c5eadae6993e9974b2a6ff207f64e7d7f3
Gustavo Ferratti <gustavo.ferratti@ipiranga.ipiranga>
2024-12-16 22:27:08 -0300

    feat: basic bgm

--- game.py
+++ game.py
@@ -6,23 +6,34 @@ from blocks import *
 
 # Config
 pygame.init()
+pygame.mixer.init()
 
+# Sound
+pygame.mixer.music.load("bgm/main_theme.mp3")
+pygame.mixer.music.set_volume(0.5)
+pygame.mixer.music.play(-1)
+
+# Screen
 screen = pygame.display.set_mode((300, 600))
 pygame.display.set_caption("Python Tetris 3000")
 
+# Clock
 clock = pygame.time.Clock()
 
+# Grid
 game_grid = Grid()
-
 block = IBlock()
 game_grid.print_grid()
 
 # Main Loop
-while True:
+game_over = False
+while not game_over:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+            pygame.mixer.music.stop()
+            game_over = True
         
         # Drawing Grid
         screen.fill(color_palette["BG"])
